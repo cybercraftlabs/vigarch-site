7223f550c8808732bac556a0f10796d12b5538d8
Gaurav Raj <techw803@gmail.com>
2024-04-07 00:48:13 +0530

    Refractored the Code

--- src/components/Blog/blogData.tsx
+++ src/components/Blog/blogData.tsx
@@ -3,45 +3,18 @@ import { Blog } from "@/types/blog";
 const blogData: Blog[] = [
   {
     id: 1,
-    title: "Best UI components for modern websites",
-    paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
     image: "/images/systemimg/1st.png",
-    author: {
-      name: "Samuyl Joshi",
-      image: "/images/systemimg/1st.png",
-      designation: "Graphic Designer",
-    },
     tags: ["Minimal and Less Bloated"],
-    publishDate: "2025",
   },
   {
     id: 2,
-    title: "",
-    paragraph:
-      "",
     image: "/images/systemimg/3rd.png",
-    author: {
-      name: "",
-      image: "",
-      designation: "",
-    },
     tags: ["Vast Tools Collections"],
-    publishDate: "2025",
   },
   {
     id: 3,
-    title: "Tips to quickly improve your coding speed.",
-    paragraph:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sit amet dictum neque, laoreet dolor.",
     image: "/images/systemimg/2nd.png",
-    author: {
-      name: "Lethium Deo",
-      image: "/images/blog/author-03.png",
-      designation: "Graphic Designer",
-    },
     tags: ["Native Tor Integration"],
-    publishDate: "2025",
   },
 ];
 export default blogData;
--- src/components/Testimonials/index.tsx
+++ src/components/Testimonials/index.tsx
@@ -1,5 +1,7 @@
 import { Testimonial } from "@/types/testimonial";
 import SectionTitle from "../Common/SectionTitle";
+import SingleBlog from "../Blog/SingleBlog";
+import blogData from "../Blog/blogData";
 import SingleTestimonial from "./SingleTestimonial";
 
 const testimonialData: Testimonial[] = [
@@ -37,14 +39,15 @@ const Testimonials = () => {
     <section className="dark:bg-bg-color-dark bg-gray-light relative z-10 py-16 md:py-20 lg:py-28">
       <div className="container">
         <SectionTitle
-          title="Frequently Asked Questions"
-          paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
+          title="Screenshots"
+          paragraph="Uncover the array of cutting-edge functionalities meticulously crafted to empower cyber warriors, security professionals, and tech enthusiasts alike."
           center
         />
-
-        <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 lg:grid-cols-3">
-          {testimonialData.map((testimonial) => (
-            <SingleTestimonial key={testimonial.id} testimonial={testimonial} />
+        <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 md:gap-x-6 lg:gap-x-8 xl:grid-cols-3">
+          {blogData.map((blog) => (
+            <div key={blog.id} className="w-full">
+              <SingleBlog blog={blog} />
+            </div>
           ))}
         </div>
       </div>
